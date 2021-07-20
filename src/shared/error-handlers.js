64c44de84bba652efef10d7333e01dcffb94a61f
Sergei-Zhukov <sergeizhukovgomel@gmail.com>
2021-07-20 21:20:17 +0300

    complete

--- src/shared/error-handlers.js
+++ src/shared/error-handlers.js
@@ -30,3 +30,47 @@ export const showErrorNotification = error => {
   body.append(notification);
   setTimeout( () => notification.style.display = 'none', 5000);
 }
+
+export const showPasswordCompareError = () => {
+  const errorTag = document.getElementById('passwordsCompareError');
+  errorTag.style.display = 'block';
+  errorTag.innerText = ERROR_MESSAGES.passwordsCompare;
+}
+
+export const hidePasswordCompareError = () => {
+  const errorTag = document.getElementById('passwordsCompareError');
+  errorTag.style.display = 'none';
+}
+
+export const showUserNameError = () => {
+  const errorTag = document.getElementById('userNameError');
+  errorTag.style.display = 'block';
+  errorTag.innerText = ERROR_MESSAGES.userName;
+}
+
+export const hideUserNameError = () => {
+  const errorTag = document.getElementById('userNameError');
+  errorTag.style.display = 'none';
+}
+
+export const showUserSurnameError = () => {
+  const errorTag = document.getElementById('userSurnameError');
+  errorTag.style.display = 'block';
+  errorTag.innerText = ERROR_MESSAGES.userSurname;
+}
+
+export const hideUserSurnameError = () => {
+  const errorTag = document.getElementById('userSurnameError');
+  errorTag.style.display = 'none';
+}
+
+export const showBirthdateError = () => {
+  const errorTag = document.getElementById('birtdateError');
+  errorTag.style.display = 'block';
+  errorTag.innerText = ERROR_MESSAGES.birth;
+}
+
+export const hideBirthdateError = () => {
+  const errorTag = document.getElementById('birtdateError');
+  errorTag.style.display = 'none';
+}
