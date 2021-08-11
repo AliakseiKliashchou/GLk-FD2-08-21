71e592f23de074891b6c5c41321138aa656896de
Dima7859 <dimade7859@gmail.com>
2021-08-11 21:10:09 +0300

    workshgadsadasd

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -1,13 +1,14 @@
 export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyD_BB_QWNm5iHT_4L15AH4JJbVgSc2yY_w',
-  authDomain: 'testapp-2a4ab.firebaseapp.com',
-  databaseURL: 'https://testapp-2a4ab-default-rtdb.firebaseio.com',
-  projectId: 'testapp-2a4ab',
-  storageBucket: 'testapp-2a4ab.appspot.com',
-  messagingSenderId: '998218434374',
-  appId: '1:998218434374:web:5aea859377ed4897544747',
+  apiKey: "AIzaSyCLFgiL1GTYAXSUxEIHWj2Sma7eKcizwNM",
+  authDomain: "dimade7859-test.firebaseapp.com",
+  databaseURL: "https://dimade7859-test-default-rtdb.firebaseio.com",
+  projectId: "dimade7859-test",
+  storageBucket: "dimade7859-test.appspot.com",
+  messagingSenderId: "709834733024",
+  appId: "1:709834733024:web:d8e4c5576ef153251ae785",
+  measurementId: "G-48QGH55PZC"
 }
 
-export const databaseURL = 'https://testapp-2a4ab-default-rtdb.firebaseio.com';
+export const databaseURL = "https://dimade7859-test-default-rtdb.firebaseio.com/";
 
 export const authUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_CONFIG.apiKey}`;
